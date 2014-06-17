ca266c54ef3dcf4598e992418ed5c196e04628c4
Seth Alves <seth@gloebit.com>
2014-06-17 16:59:52 -0700

    sync

--- tests/srfi-13-strings/test-common.scm
+++ tests/srfi-13-strings/test-common.scm
@@ -138,8 +138,23 @@
 
         )
 
-    (and t0 t1 t2 t3 t4 t5 t6
-         t7 ;; fails on chicken (maybe fixed now)
-         t8 t9 t10 t11 t12 t13 t14 t15 t16)
+    (and t0
+         t1
+         t2
+         t3
+         t4
+         t5
+         t6
+         t7
+         t8
+         t9
+         t10
+         t11
+         t12
+         t13
+         t14
+         t15
+         t16
+         )
 
     ))
