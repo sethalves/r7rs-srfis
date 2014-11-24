b8516cd616af2825b8be4db8d3c023b14d5a57fb
Seth Alves <seth.alves@gmail.com>
2014-11-23 16:47:41 -0800

    trying to get kawa tests to work

---
+++ srfi-tests/1/test-kawa.scm
@@ -0,0 +1,12 @@
+#! /bin/sh
+#| -*- scheme -*-
+exec /usr/local/bin/kawa \
+  -Dkawa.import.path="./*.sld" \
+  $0 "$@"
+|#
+
+(import (scheme base)
+        (scheme write)
+        (srfi 1 tests))
+(display (run-tests))
+(newline)
